ccca569c5684371ff9ec21ab8a63ea1132b8a5a7
itgimpi <brejkersi@gmail.com>
2023-10-25 14:07:08 +0200

    prolog

---
+++ 1Program/p1.pl
@@ -0,0 +1,26 @@
+/*
+Prolog Program
+
+kompajliranje: cl(ime programa)
+
+
+*/
+
+mis(dzeri).
+macka(tom).
+
+zivotinja(X) :- mis(X).
+
+/*
+
+Mis Zivotinja
+0   0
+0   1
+1   0    Ne sme da se desi
+1   1
+
+
+*/
+
+   
+
---
+++ 1Program/p2.pl
@@ -0,0 +1,38 @@
+/*
+Prolog Program
+Terms
+
+Types of Terms:
+
+1. Numbers - Brojevi
+2. Atoms - Atom
+3. Variables - Promenljive
+4. Compound Terms - 
+5. Lists - Liste, nizovi
+
+Numbers - Integer
+*/
+
+go :- 
+    Broj is 5,
+    writeln(Broj).
+
+go2 :- 
+    Broj2 is 2'101,
+    Broj3 is 16'ABC,
+    Broj4 is 3'12,
+    Broj5 is 0'a,
+    writeln(Broj2),
+    writeln(Broj3),
+    writeln(Broj4),
+    writeln(Broj5).
+
+/*Numbers - Float*/
+
+go3 :- 
+    Pi is 3.14,
+    writeln(Pi).
+
+
+   
+
