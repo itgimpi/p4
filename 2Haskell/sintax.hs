c345b3b7fb4cd8634a484d335bb110f0648abc9d
itgimpi <brejkersi@gmail.com>
2024-03-06 12:49:48 +0100

    Update sintax.hs

--- 2Haskell/sintax.hs
+++ 2Haskell/sintax.hs
@@ -22,3 +22,27 @@ ocena 5 = "pet"
 ocena x = "mora da bude od 1 do 5" --mora da bude na kraju (else)
 
 
+
+
+
+
+
+
+
+fact :: Integer -> Integer
+fact 0 = 1 --BC
+fact n = n * fact(n-1) --Rec
+
+addVector :: (Int, Int)->(Int, Int)->(Int, Int)
+addVector a b = ( fst a + fst b, snd a + snd b)
+
+addVector' :: (Int, Int)->(Int, Int)->(Int, Int)
+addVector' (x1,y1) (x2,y2) = (x1+x2, y1+y2)
+
+head' :: [a] -> a
+head' [] = error "Prazna lista!"
+head'(x:_)=x
+
+length' :: [a]->Int
+length' [] = 0
+length' (_:xs) = 1 + length' xs
